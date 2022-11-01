c7e687bf74465c25b92980edd32f4b56c45f82ef
marie seaman <marie.seaman1202@gmail.com>
2022-11-02 01:11:05 +0900

    first if-statement added for users to interact with

--- main.rb
+++ main.rb
@@ -1,4 +1,10 @@
 puts "What would you like to ask?"
 message = gets.chomp
 
-puts message
+if message.include?("hello")
+  puts "Hello to you too!"
+elsif message.downcase.include?("dinner")
+  puts ["tacos", "sandwich", "sushi", "pizza", "pasta", "kebab"].sample
+else
+  puts "Sorry, I don't know that..."
+end
